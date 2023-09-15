8095c7befe33368d927c48d86289125b7f736ed7
Jamie <espiritu_jamie@hotmail.com>
2023-09-15 09:47:35 -0600

    Created StudentInfo

--- app/StudentInfo/page.js
+++ app/StudentInfo/page.js
@@ -3,11 +3,16 @@ import Link from "next/link";
 export default function StudentInfo() {
   return (
     <main>
-      <h1>Name:</h1>
-      <p>Jamie Espiritu</p>
-      <h1>Course Section:</h1>
-      <p>CPRG 306-C</p>
-      <Link href="/"> </Link>
+      <h1>
+        <b>Name:</b> Jamie Espiritu
+      </h1>
+      <h1>
+        <b>Course Section:</b> CPRG-306-C
+      </h1>
+      <h1>
+        <b>GitHub Assignment Repository:</b>
+        <Link href="https://github.com/espiritu-jamie/cprg306-assignments">Jamie Espiritu - GitHub Assignment Repository</Link>
+      </h1>
     </main>
   )
 }
