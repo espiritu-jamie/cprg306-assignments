8030248173cf46352422759519b549fe90d6e38b
Jamie <espiritu_jamie@hotmail.com>
2023-09-15 10:05:53 -0600

    Added StudentInfo to Root Page

--- app/page.js
+++ app/page.js
@@ -1,6 +1,15 @@
+import Link from "next/link";
+import StudentInfo from "./StudentInfo/page";
+
 export default function Home() {
   return (
-    <main> Hello!
+    <main>
+      <h1>
+        CPRG 306: Web Development 2 - Assignments
+      </h1>
+      <StudentInfo />
+      <Link href="/week2">Here is the link to week 2</Link>
+
     </main>
   )
 }
