6f2a63e00dbfcb12ddd718d73ad072ff48a3090f
Jamie <espiritu_jamie@hotmail.com>
2023-09-15 09:40:25 -0600

    Created Home

---
+++ app/StudentInfo/page.js
@@ -0,0 +1,13 @@
+import Link from "next/link";
+
+export default function StudentInfo() {
+  return (
+    <main>
+      <h1>Name:</h1>
+      <p>Jamie Espiritu</p>
+      <h1>Course Section:</h1>
+      <p>CPRG 306-C</p>
+      <Link href="/"> </Link>
+    </main>
+  )
+}
