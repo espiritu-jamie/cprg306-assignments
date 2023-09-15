ca9535b20ac95122dad550856ccb4e6f7b170795
Jamie <espiritu_jamie@hotmail.com>
2023-09-15 10:42:13 -0600

    Added Home to Week2

--- app/week2/page.js
+++ app/week2/page.js
@@ -1,10 +1,12 @@
 import StudentInfo from "../StudentInfo";
+import Link from "next/link";
 
 export default function Home() {
     return(
         <main>  
             <h1><b>My Shopping List</b></h1>
             <StudentInfo />
+            <Link href="/">Back to Home</Link>
         </main>
     )
 }
