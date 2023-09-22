b723288444655990a15d0d4f5808653fcf0b9aec
Jamie <espiritu_jamie@hotmail.com>
2023-09-22 10:20:00 -0600

    edit font size

--- app/week3/item.js
+++ app/week3/item.js
@@ -1,7 +1,7 @@
 export default function Item({name, quantity, category}) {
   return (
     <ul>
-        <li className="text-lg m-4 p-2 bg-slate-700 text-slate-100 w-9/12-max">{name}
+        <li className="text-lg m-4 p-2 bg-slate-700 text-slate-100 w-5/12">{name}
         <p className="text-sm">Buy {quantity} in {category}</p>
         </li>
     </ul>
