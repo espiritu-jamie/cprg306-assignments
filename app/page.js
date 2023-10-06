77552fa19d0b8c1203e42fcd8637c8bd728b87b0
Jamie <espiritu_jamie@hotmail.com>
2023-10-06 13:05:43 -0600

    Edit font style

--- app/page.js
+++ app/page.js
@@ -4,11 +4,12 @@ import StudentInfo from "./StudentInfo";
 export default function Home() {
   return (
     <main>
-      <h1 className="text-2xl font-bold">CPRG 306: Web Development 2 - Assignments</h1>
+      <h1 className="text-center text-2xl font-bold">CPRG 306: Web Development 2 - Assignments</h1>
       <StudentInfo />
-      <ul>
+      <ul className="text-center m-10">
         <li><Link href="/week2">Week 2</Link></li>
         <li><Link href="/week3">Week 3</Link></li>
+        <li><Link href="/week4">Week 4</Link></li>
       </ul>
       
 
