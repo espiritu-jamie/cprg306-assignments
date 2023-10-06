6e8167f067090a20fac4c687826eb8a37cc377f8
Jamie <espiritu_jamie@hotmail.com>
2023-10-06 13:05:52 -0600

    Edit font style

--- app/StudentInfo.js
+++ app/StudentInfo.js
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function StudentInfo() {
   return (
-    <main>
+    <main className="m-8">
         <h1>Name: Jamie Espiritu</h1>
         <h1>Course Section: CPRG 306-C</h1>
         <Link href="https://github.com/espiritu-jamie/cprg306-assignments">My link to GitHub Repository Assignments</Link>
